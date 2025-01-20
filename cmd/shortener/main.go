81298bfb17115c7af5abedcf00ce779e0323fc9d
Nikita Kamchatkin <nkamchatkin@bionlab.ru>
2025-01-20 19:46:05 +0300

    iter6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,28 +2,19 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-chi/chi/v5"
 	"github.com/kamchatkin/practicum-shortener/config"
-	"github.com/kamchatkin/practicum-shortener/internal/app"
+	"github.com/kamchatkin/practicum-shortener/internal/router"
 	"net/http"
 )
 
 func main() {
-	r := chi.NewRouter()
-
-	// Сокращение
-	r.Post("/", app.SynonymHandler)
-
-	// Переадресация
-	r.Get("/{id}", app.RedirectHandler)
-
 	cfg, err := config.Config()
 	if err != nil {
 		fmt.Printf("Ошибка подготовки конфигурации приложения. Надо ли в этом случае давать запускать приложение?\n%s", err)
 		panic(err)
 	}
 
-	if err := http.ListenAndServe(cfg.Addr, r); err != nil {
+	if err := http.ListenAndServe(cfg.Addr, router.Router()); err != nil {
 		panic(err)
 	}
 }
