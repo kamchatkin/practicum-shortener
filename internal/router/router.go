3a320b843a08678a95733f09c7432184b564a837
Nikita Kamchatkin <nkamchatkin@bionlab.ru>
2025-01-27 02:29:56 +0300

    Компрессия chi

--- internal/router/router.go
+++ internal/router/router.go
@@ -2,8 +2,8 @@ package router
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/kamchatkin/practicum-shortener/internal/app"
-	"github.com/kamchatkin/practicum-shortener/internal/router/middlewares/gzip"
 	"github.com/kamchatkin/practicum-shortener/internal/router/middlewares/log"
 	"net/http"
 )
@@ -11,6 +11,8 @@ import (
 // Router Маршруты приложения
 func Router() *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Compress(5, ""))
 
 	// Сокращение
 	r.Post("/", handleWrapper(app.SynonymHandler))
@@ -27,7 +29,8 @@ func Router() *chi.Mux {
 // handleWrapper обертка хэндлеров в мидлвари
 func handleWrapper(next http.HandlerFunc) http.HandlerFunc {
 	mwsList := []func(next http.HandlerFunc) http.HandlerFunc{
-		log.WithLogging, gzip.WithGzipped,
+		log.WithLogging,
+		//gzip.WithGzipped,
 	}
 
 	for _, middleware := range mwsList {
